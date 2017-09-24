95b9b6f1253ff120517bea91e231d73a0431a7b0
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2017-09-24 19:42:51 +0200

    [3.5.0] Add random sort, support keep as unread

--- script/Feedly_filtering_and_sorting.user.js
+++ script/Feedly_filtering_and_sorting.user.js
@@ -14,7 +14,7 @@
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js
 // @resource    jscolor.js https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js
 // @include     *://feedly.com/*
-// @version     3.4.0
+// @version     3.5.0
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_deleteValue
