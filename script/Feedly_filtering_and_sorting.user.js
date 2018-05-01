507fe624f61d8492b7b0d0176f810197cee512da
Soufiane Sakhi <soufiane.sakhi.js@gmail.com>
2018-05-01 11:45:09 +0200

    [3.11.1] Minor bug fix

--- script/Feedly_filtering_and_sorting.user.js
+++ script/Feedly_filtering_and_sorting.user.js
@@ -14,7 +14,7 @@
 // @resource    node-creation-observer.js https://greasyfork.org/scripts/19857-node-creation-observer/code/node-creation-observer.js?version=174436
 // @require     https://cdnjs.cloudflare.com/ajax/libs/jscolor/2.0.4/jscolor.min.js
 // @include     *://feedly.com/*
-// @version     3.11.0
+// @version     3.11.1
 // @grant       GM_setValue
 // @grant       GM_getValue
 // @grant       GM_deleteValue
